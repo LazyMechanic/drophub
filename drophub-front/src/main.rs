3ba1cf94cb5912ad3864f0c77cf558542788b64f
LazyMechanic <asharnrus@gmail.com>
2023-04-28 11:36:58 +0300

    Add logging

--- drophub-front/src/main.rs
+++ drophub-front/src/main.rs
@@ -1,7 +1,27 @@
 mod app;
 
 use app::App;
+use tracing_subscriber::{
+    fmt::{format::Pretty, time::UtcTime},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
+use tracing_web::{performance_layer, MakeConsoleWriter};
 
 fn main() {
+    init_logging();
     yew::Renderer::<App>::new().render();
+}
+
+fn init_logging() {
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(false) // Only partially supported across browsers
+        .with_timer(UtcTime::rfc_3339()) // std::time is not available in browsers
+        .with_writer(MakeConsoleWriter); // write events to the console
+    let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
+
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(perf_layer)
+        .init(); // Install these as subscribers to tracing events
 }
